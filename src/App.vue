79eba4ce706762836a48422d23cecc012895dd69
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2021-07-03 19:13:37 +0100

    Initial Commit  / Hash Uploaded Files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+        <input type="file" ref="uploadedfile" @change="fileUpload($event)">
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/X';
+import cryptoJS from 'crypto-js';
+import sha256 from 'crypto-js/sha256';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    //X
+  },
+
+  data(){
+    return {
+      //
+    }
+  },
+
+  methods:{
+    fileUpload(e) {
+      const filesToRead = e.target.files;
+      let file1 = filesToRead[0];
+      const fileReader = new FileReader();
+      fileReader.addEventListener('loadend', (evt) => {
+        if (evt.target.readyState == FileReader.DONE) {
+          file1 = fileReader.result;
+          const encryptedvalue = sha256(cryptoJS.enc.Latin1.parse(file1)).toString();
+          console.log("SHA256 value is :" + encryptedvalue);
+        }      
+      });
+      fileReader.readAsBinaryString(file1); // missing in the posted code
+    }
+  }
+};
+</script>
