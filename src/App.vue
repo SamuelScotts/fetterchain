41c32550bf2a205853a515d405002486b5cf19e2
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2021-07-03 20:18:50 +0100

    Commented / Slight Styling Added / Beginning 'Adding Blocks'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <v-app>
     <v-main>
-        <input type="file" ref="uploadedfile" @change="fileUpload($event)">
+        <v-container class="grey lighten-5">
+          <v-row no-gutters>
+            
+            <!-- Blank holder column -->
+            <v-col cols="12" sm="2"></v-col>
+            
+            <!-- Uploads occur on change i.e when image is selected.  File number increments each time -->
+            <v-col cols="12" sm="8">
+              <input type="file" ref="uploadedfile" @change="fileUpload($event)">
+              <h2>Files added: {{filesToBeAdded.length}}</h2>
+              
+              <!-- Only appears when images available to add -->
+              <v-btn v-if="filesToBeAdded.length > 0">Submit to network</v-btn>
+            </v-col>
+            
+            <!-- Blank holder column -->
+            <v-col cols="12" sm="2"></v-col>
+          </v-row>
+        </v-container>
     </v-main>
   </v-app>
 </template>
@@ -20,11 +38,13 @@ export default {
 
   data(){
     return {
-      //
+      // Array to hold files to be uploaded in next block.
+      filesToBeAdded: []
     }
   },
 
   methods:{
+    // Handle uploading of each image.  Currently done one at a time.
     fileUpload(e) {
       const filesToRead = e.target.files;
       let file1 = filesToRead[0];
@@ -32,12 +52,17 @@ export default {
       fileReader.addEventListener('loadend', (evt) => {
         if (evt.target.readyState == FileReader.DONE) {
           file1 = fileReader.result;
+          // Running from Crypto-JS package, using SHA256 algorithm
           const encryptedvalue = sha256(cryptoJS.enc.Latin1.parse(file1)).toString();
-          console.log("SHA256 value is :" + encryptedvalue);
+          this.filesToBeAdded.push(encryptedvalue);
         }      
       });
-      fileReader.readAsBinaryString(file1); // missing in the posted code
-    }
+      fileReader.readAsBinaryString(file1);
+    },
+    // Add images which have ben uploaded.
+    addImages(){
+      
+    },
   }
 };
 </script>
