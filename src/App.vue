e6e52a260e9b2001cb11e46bdae8edd6deda557f
Samuel Scott <66216873+SamuelScotts@users.noreply.github.com>
2021-07-03 21:20:04 +0100

    Work in progress re Blockchain

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
               <h2>Files added: {{filesToBeAdded.length}}</h2>
               
               <!-- Only appears when images available to add -->
-              <v-btn v-if="filesToBeAdded.length > 0">Submit to network</v-btn>
+              <v-btn v-if="filesToBeAdded.length > 0" @click="addImages()">Submit to network</v-btn>
             </v-col>
             
             <!-- Blank holder column -->
@@ -38,6 +38,8 @@ export default {
 
   data(){
     return {
+      // Main ledger holder.
+      ledger: [],
       // Array to hold files to be uploaded in next block.
       filesToBeAdded: []
     }
@@ -55,13 +57,33 @@ export default {
           // Running from Crypto-JS package, using SHA256 algorithm
           const encryptedvalue = sha256(cryptoJS.enc.Latin1.parse(file1)).toString();
           this.filesToBeAdded.push(encryptedvalue);
+          console.log(encryptedvalue)
         }      
       });
       fileReader.readAsBinaryString(file1);
     },
+    compareUploadToExisting(){
+      for(let i=0; i<this.filesToBeAdded.length;i++){
+        for (let j=0; j<this.ledger.length;j++){
+          if(this.filesToBeAdded[i] === this.ledger[j]){
+            alert(this.filesToBeAdded[i] + " already exists")
+          }
+        }
+      }
+    },
     // Add images which have ben uploaded.
     addImages(){
-      
+      // OBTAIN LEDGER AND UPDATE!
+      // Run comparision between images to be uploaded and existing images.
+      this.compareUploadToExisting()
+
+      let timestamp = Date.now();
+      let previousHash = this.ledger.length
+      if (this.ledger.length == 0){
+        previousHash = 0;
+      }
+      let 
+
     },
   }
 };
